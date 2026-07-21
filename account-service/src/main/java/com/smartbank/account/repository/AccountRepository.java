c4e0d15a378c06d1380a1085ae86d175530d1a62
Kashish Prasad <kashishpra1504@gmail.com>
2026-07-21 17:21:24 +0530

    Added account repository

--- account-service/src/main/java/com/smartbank/account/repository/AccountRepository.java
+++ account-service/src/main/java/com/smartbank/account/repository/AccountRepository.java
@@ -5,9 +5,7 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 
 import java.util.List;
 
-// Data access for accounts (PRD sec 6.12).
 public interface AccountRepository extends MongoRepository<Account, String> {
 
-    // GET /accounts/customer/{customerId} - a customer can hold multiple accounts.
     List<Account> findByCustomerId(String customerId);
 }
